d6440812b7ad1223817e53738bd4749d8033d604
Kimberly Ly <kily@microsoft.com>
2019-02-28 12:02:18 -0800

    Fixed typos in stories. Typo in config was causing a story to load improperly.

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -4,7 +4,7 @@ import TwoWayQuerybuilder from '../../src/TwoWayQuerybuilder';
 import './index.css';
 
 const config = {
-  query: "((firstname >= 'Jack' AND lastName<'London') OR lastName<>'Smith')",
+  query: "((firstName >= 'Jack' AND lastName<'London') OR lastName<>'Smith')",
 };
 
 const dropdownconfig = {
@@ -24,9 +24,9 @@ const validationFields = [
   { name: 'lastName', operators: 'all', label: 'Last Name', input: {
      type: 'text', errorText: 'Only letters allowed', pattern: new RegExp("[a-z]+", "gi") } },
   { name: 'age', operators: 'all', label: 'Age', input: {
-     type: 'text', errorText: 'Only nubmers allowed', pattern: new RegExp('[0-9]+', 'gi') } },
+     type: 'text', errorText: 'Only numbers allowed', pattern: new RegExp('[0-9]+', 'gi') } },
   { name: 'birthDate', operators: 'all', label: 'Birth date', input: { 
-    type: 'text', errorText: 'Only nubmers allowed', pattern: new RegExp('[0-9]+', 'gi') }
+    type: 'text', errorText: 'Only numbers allowed', pattern: new RegExp('[0-9]+', 'gi') }
    },
 ];
 
